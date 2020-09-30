adf83a4df938749ff266975030d36e7c07367ba6
Josh Gubler <josh.gubler@gmail.com>
2020-09-30 13:52:11 -0600

    ci TF 12 and 13

---
+++ examples/ci-13/ci.tf
@@ -0,0 +1,25 @@
+terraform {
+  required_version = "0.13.2"
+}
+
+provider "aws" {
+  version = "~> 3.0"
+  region  = "us-west-2"
+}
+
+module "ecr" {
+  source = "../../"
+  name   = "example-repo"
+}
+
+output "repository" {
+  value = module.ecr.repository
+}
+
+output "lifecycle_policy" {
+  value = module.ecr.lifecycle_policy
+}
+
+output "repository_policy" {
+  value = module.ecr.repository_policy
+}
